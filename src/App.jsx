d8f3dec9a612a85e372b2e45fe2e0e20af9c4222
Thaviab <abthaveesh@gmail.com>
2025-05-01 09:10:43 +1000

    Push latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { NotFound } from "./pages/NotFound"
+import { Home } from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    
+    {/* //defining routes */}
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />}/> {/* Everything that is not defined will go here */}
+      </Routes>
+    </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
